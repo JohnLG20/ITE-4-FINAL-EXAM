a64e20428cc72b65a1dbec3d2f3734dfed8a41dd
JohnLG20 <87460733+JohnLG20@users.noreply.github.com>
2023-01-12 05:31:17 +0800

    Add files via upload

---
+++ rps_sumalinog/rps_game/views.py
@@ -0,0 +1,64 @@
+# Django Import
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+
+
+# Python Import
+import random
+
+# Create your views here.
+def Homepage(request):
+    if request.POST.get('start',''):
+        return redirect('Game')
+    return render(request, 'index.html')
+
+def Game(request):
+    gamelist = ['rock', 'paper', 'scissors']
+    bot_action = random.choice(gamelist)
+    if request.method == 'POST':
+        user_answer = request.POST.get('name','')
+
+        if user_answer == bot_action:
+            messages.info(request, "Both players selected. It's a tie!")
+            return redirect('Game')
+
+        elif user_answer == "rock":
+            if bot_action == "scissors":
+                messages.info(request,"Rock and Scissors.. You win!")
+                return redirect('Game')
+               
+            else:
+               
+                messages.info(request,"Computer Choose Paper! You lose.")
+                return redirect('Game')
+
+                
+
+        elif user_answer == "paper":
+            if bot_action == "rock":
+
+                messages.info(request,"Paper and Rock.. You win!")
+                return redirect('Game')
+                
+            else:
+                
+                messages.info(request,"Computer Choose Scissors! You lose.")
+                return redirect('Game')
+                
+
+        elif user_answer == "scissors":
+            if bot_action == "paper":
+               
+                messages.info(request,"Scissor and Paper.. You win!")
+                return redirect('Game')
+                
+            else:
+                messages.info(request,"Computer Choose Rock! You lose.")
+                return redirect('Game')
+
+    
+                
+    
+    return render(request, 'game.html')
